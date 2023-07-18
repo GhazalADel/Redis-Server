950f1d4ef7bf2249afc78c8e6331c3175627e6c5
GhazalADel <gashoftedel@aut.ac.ir>
2023-07-18 17:10:26 +0330

    PONG implementation

---
+++ README.md
@@ -0,0 +1,4 @@
+to run the server, use this command:\
+    ./spawn_redis_server.sh
+in another terminal:\
+    redis-cli -h 127.0.0.1 -p 6390
--- app/server.go
+++ app/server.go
@@ -6,16 +6,49 @@ import (
 	"os"
 )
 
-func main() {
+const (
+	PORT    string = "6390"
+	IP      string = "127.0.0.1"
+	ADDRESS string = IP + ":" + PORT
+)
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+func main() {
+	listener, err := net.Listen("tcp", ADDRESS)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Printf("Failed to bind to port %s", PORT)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+
+	fmt.Println("Server started. Listening on", ADDRESS)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 128)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading command: ", err.Error())
+		}
+
+		fmt.Printf("Input Command is : %s\n", buf[:n])
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing command: ", err.Error())
+		}
+	}
+
 }
